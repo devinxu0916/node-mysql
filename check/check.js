156eb3361d5d7b9d7ac463e627ee12d0c117a1f1
ontheway-baiye <1905068780.@qq.com>
2017-09-27 17:15:49 +0800

    检查模块

---
+++ check/check.js
@@ -0,0 +1,13 @@
+const user = require('../models/mongo');
+
+let find = function (name) {
+    let flag = false;
+    user.find({name: name}, (err, data) => {
+        if (data) {
+            flag = true
+        }
+        return flag;
+    })
+};
+
+module.exports = find;
